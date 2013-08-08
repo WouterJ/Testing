f05166fa6c5a3aa9103be8a1b4cc656f4c331f0d
WouterJ <waldio.webdesign@gmail.com>
2013-08-08 19:26:49 +0200

    Removed command conflict

--- bin/travis/phpcr_odm_doctrine_orm_dbal.sh
+++ bin/travis/phpcr_odm_doctrine_orm_dbal.sh
@@ -6,3 +6,4 @@ CONSOLE_DIR=$DIR_NAME"/.."
 # composer install --dev
 php $CONSOLE_DIR"/console" doctrine:database:create --env=orm
 php $CONSOLE_DIR"/console" doctrine:schema:create --env=orm
+php $CONSOLE_DIR"/console" doctrine:phpcr:repository:init
